c09e9d4af144af6378cc7161a5fc779b3b913b23
Vitalij Neverov <vitalij.neverov@gmail.com>
2026-02-03 11:04:41 +0100

    Add posts endpoints GET, POST and GET by id

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,44 @@ app.post('/users', async (req: Request, res: Response) => {
   res.status(201).json({ user });
 });
 
+//GET posts
+app.get('/posts', async (req: Request, res: Response) => {
+  const posts = await prisma.post.findMany({ include: { author: true } });
+
+  res.status(200).json({ posts });
+});
+
+//POST create post
+app.post('/posts', async (req: Request, res: Response) => {
+  const { title, content, authorId } = req.body;
+
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+      authorId: Number(authorId),
+    },
+  });
+
+  res.status(201).json({ post });
+});
+
+//GET post with Id
+app.get('/posts/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      author: true,
+    },
+  });
+
+  res.status(200).json({ post });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
